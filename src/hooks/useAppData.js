cbfe2e17d2662522b85542ca2b24abbf31e0a557
Mulligascos <53591720+Mulligascos@users.noreply.github.com>
2026-03-04 13:32:57 +1300

    Add files via upload

--- src/hooks/useAppData.js
+++ src/hooks/useAppData.js
@@ -64,7 +64,7 @@ export const useAppData = (currentUser) => {
   return {
     courses, tournaments, matches, players,
     isLoading, lastLoaded, loadData,
-    activeTournament,
+    activeTournament, pendingRequestsCount,
     setCourses, setTournaments, setMatches, setPlayers,
   };
 };
